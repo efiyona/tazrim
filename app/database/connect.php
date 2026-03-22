e4703a42fc52a3bdea48cab212daffc20a17af9a
Efi Yona <efiyona10@gmail.com>
2026-03-22 20:14:12 +0200

    שינוי לפני העלאה לאוויר

--- app/database/connect.php
+++ app/database/connect.php
@@ -1,14 +1,9 @@
 <?php
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-$host = 'localhost';
-$user = 'root';
-$pass = '';
-$db_name = 'tazrim';
+    if($conn->connect_error){
+        die('Data Base Connection error: ' . $conn->connect_error);
+    }
 
-$conn = new mysqli($host, $user, $pass, $db_name);
-
-if($conn->connect_error){
-    die('Data Base Connection error: ' . $conn->connect_error);
-}
-
-date_default_timezone_set('Asia/Jerusalem');
+    date_default_timezone_set('Asia/Jerusalem');
+?>
--- path.php
+++ path.php
@@ -1,5 +1,9 @@
 <?php 
 define("ROOT_PATH", realpath(dirname(__FILE__)));
-define("BASE_URL", "https://localhost/tazrim/");
 
+// טוענים את קובץ הסודות כדי להשתמש במשתנים שבו
+require_once(ROOT_PATH . '/secrets.php');
+
+// משתמשים בקבוע שהגדרנו בסודות
+define("BASE_URL", SITE_URL);
 ?>
